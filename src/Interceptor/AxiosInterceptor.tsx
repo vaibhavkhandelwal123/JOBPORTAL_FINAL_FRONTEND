490c2faa2210d91b9aae58f3a49f9e3f91c3ba3d
khandelwalvaibhav123 <khandelwalvaibhav123456@gmail.com>
2025-08-04 11:27:04 +0530

    remove user

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -23,6 +23,8 @@ export const setUpResponseInterceptor = (navigate:any) =>{
         },
         (error) => {
             if (error.response && error.response.status === 401) {
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
                 navigate("/login");
             }
             return Promise.reject(error);
